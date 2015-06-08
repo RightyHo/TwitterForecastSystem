0f3612293269cae12c9db2c380e2b01cb218919f
Andrew Ho <andrewyc.ho@gmail.com>
2015-06-08 22:51:59 +0100

    First commit
    Started a brief skeleton of the data extraction package

---
+++ TFS/src/dataextraction/Tweet.java
@@ -0,0 +1,8 @@
+package dataextraction;
+
+/**
+ * Created by Andrew on 08/06/15.
+ */
+public interface Tweet {
+
+}
---
+++ TFS/src/dataextraction/TweetExtractor.java
@@ -0,0 +1,15 @@
+package dataextraction;
+
+/**
+ * Created by Andrew on 08/06/15.
+ */
+public interface TweetExtractor {
+    /*
+     *  Extract raw tweets from twitter corpus file and store them ready for processing
+     */
+    void extractRawTweets();
+
+    /*
+     *
+     */
+}
