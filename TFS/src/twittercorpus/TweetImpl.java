435d982de35e9e2b8b9f4143889f1c2bea6e9224
Andrew Ho <andrewyc.ho@gmail.com>
2015-06-16 22:03:16 +0100

    created some of the constructors, getters and setters for the TweetImpl class

--- TFS/src/twittercorpus/TweetImpl.java
+++ TFS/src/twittercorpus/TweetImpl.java
@@ -10,13 +10,106 @@ public class TweetImpl implements Tweet {
     private boolean isRetweet;
     private boolean isLabelled;
     private boolean isPublishedOutsideMarketHours;
-    private String rawTweetText;                        // consolidate into one over-written variable?
-    private String unabbreviatedTweetText;              // consolidate into one over-written variable?
-    private String spellCheckedUnabbreviatedTweetText;  // consolidate into one over-written variable?
+    private String tweetText;       // split into three variables to separate unabbreviated and spell checked states?
     private PriceSnapshot priceSnapshot;
     private Tweet nextTweet;
     private Sentiment sentiment;    // sentiment object is initially set to unclassified.  Once classified it is set to positive, negative or neutral.
 
+    // constructors
+
+    public TweetImpl(ZonedDateTime timeStamp, boolean isPublishedOutsideMarketHours, String tweetText) {
+        this.timeStamp = timeStamp;
+        this.isRetweet = false;
+        this.isLabelled = false;
+        this.isPublishedOutsideMarketHours = isPublishedOutsideMarketHours;
+        this.tweetText = tweetText;
+        this.priceSnapshot = null;
+        this.nextTweet = null;
+        this.sentiment = Sentiment.UNCLASSIFIED;
+    }
+
+    public TweetImpl(ZonedDateTime timeStamp, boolean isRetweet, boolean isLabelled, boolean isPublishedOutsideMarketHours, String tweetText, PriceSnapshot priceSnapshot, Tweet nextTweet, Sentiment sentiment) {
+        this.timeStamp = timeStamp;
+        this.isRetweet = isRetweet;
+        this.isLabelled = isLabelled;
+        this.isPublishedOutsideMarketHours = isPublishedOutsideMarketHours;
+        this.tweetText = tweetText;
+        this.priceSnapshot = priceSnapshot;
+        this.nextTweet = nextTweet;
+        this.sentiment = sentiment;
+    }
+
+    // getters and setters
+
+    public ZonedDateTime getTimeStamp() {
+        return timeStamp;
+    }
+
+    public void setTimeStamp(ZonedDateTime timeStamp) {
+        this.timeStamp = timeStamp;
+    }
+
+    public boolean isRetweet() {
+        return isRetweet;
+    }
+
+    public void setIsRetweet(boolean isRetweet) {
+        this.isRetweet = isRetweet;
+    }
+
+    public boolean isLabelled() {
+        return isLabelled;
+    }
+
+    public void setIsLabelled(boolean isLabelled) {
+        this.isLabelled = isLabelled;
+    }
+
+    public boolean isPublishedOutsideMarketHours() {
+        return isPublishedOutsideMarketHours;
+    }
+
+    public void setIsPublishedOutsideMarketHours(boolean isPublishedOutsideMarketHours) {
+        this.isPublishedOutsideMarketHours = isPublishedOutsideMarketHours;
+    }
+
+    public String getTweetText() {
+        return tweetText;
+    }
+
+    public void setTweetText(String tweetText) {
+        this.tweetText = tweetText;
+    }
+
+    public PriceSnapshot getPriceSnapshot() {
+        return priceSnapshot;
+    }
+
+    public void setPriceSnapshot(PriceSnapshot priceSnapshot) {
+        this.priceSnapshot = priceSnapshot;
+    }
+
+    public Tweet getNextTweet() {
+        return nextTweet;
+    }
+
+    public void setNextTweet(Tweet nextTweet) {
+        this.nextTweet = nextTweet;
+    }
+
+    public Sentiment getSentiment() {
+        return sentiment;
+    }
+
+    public void setSentiment(Sentiment sentiment) {
+        this.sentiment = sentiment;
+    }
+
+    /**
+     * labels the tweet by setting the price snapshot that corresponds to the timestamp of the tweet as well as
+     * setting the sentiment value of the tweet and marking the isLabelled flag as true
+     * @param priceSnapshot
+     */
     public void labelTweet(PriceSnapshot priceSnapshot){
 
     }
