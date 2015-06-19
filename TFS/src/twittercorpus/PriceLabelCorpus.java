c4829db2788701075a943810e92c9fdb2b70e003
Andrew Ho <andrewyc.ho@gmail.com>
2015-06-19 19:48:34 +0100

    wrote code for the PriceLabelCorpus Interface

--- TFS/src/twittercorpus/PriceLabelCorpus.java
+++ TFS/src/twittercorpus/PriceLabelCorpus.java
@@ -1,9 +1,20 @@
 package twittercorpus;
 
+import java.time.ZonedDateTime;
+import java.util.Map;
+
 /**
  * Created by Andrew on 16/06/15.
  */
 public interface PriceLabelCorpus {
+
+    Map<ZonedDateTime, PriceSnapshot> getPriceMap();
+
+    void setPriceMap(Map<ZonedDateTime, PriceSnapshot> priceMap);
+
+    String getFileName();
+
+    void setFileName(String fileName);
 
     void extractPriceDataFromFile(String fileName);
 
