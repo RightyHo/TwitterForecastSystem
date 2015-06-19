15f95218358c4ebb63d30eb212944491ea602eeb
Andrew Ho <andrewyc.ho@gmail.com>
2015-06-19 21:39:28 +0100

    wrote code for the labelCorpus() method in the TwitterCorpusListImpl class

--- TFS/src/twittercorpus/PriceLabelCorpus.java
+++ TFS/src/twittercorpus/PriceLabelCorpus.java
@@ -18,5 +18,4 @@ public interface PriceLabelCorpus {
 
     void extractPriceDataFromFile(String fileName);
 
-
 }
--- TFS/src/twittercorpus/TwitterCorpusListImpl.java
+++ TFS/src/twittercorpus/TwitterCorpusListImpl.java
@@ -16,6 +16,11 @@ public class TwitterCorpusListImpl implements TwitterCorpus {
 
     private static final String usernameEquivalenceToken = "USERNAME";
     private static final String linkEquivalenceToken = "LINK";
+    private LocalTime bmwXetraOpen = LocalTime.of(8,0,0);		// London time
+    private LocalTime bmwXetraClose = LocalTime.of(16,35,0);	// London time
+    private LocalTime bmwUSOTCOpen = LocalTime.of(14,30,0);		// London time
+    private LocalTime bmwUSOTCClose = LocalTime.of(21, 0, 0);	// London time
+
     private List<Tweet> corpus;
     private String fileName;
 
@@ -76,11 +81,6 @@ public class TwitterCorpusListImpl implements TwitterCorpus {
 
                 // check if the tweet was published during BMW stock market trading hours.
                 // use OffsetTime class if subtraction doesn't work using LocalTime objects.
-
-                LocalTime bmwXetraOpen = LocalTime.of(8,0,0);		// London time
-                LocalTime bmwXetraClose = LocalTime.of(16,35,0);	// London time
-                LocalTime bmwUSOTCOpen = LocalTime.of(14,30,0);		// London time
-                LocalTime bmwUSOTCClose = LocalTime.of(21, 0, 0);		// London time
 
                 boolean tsOutOfXetraMarketHours = (ts.toLocalTime().compareTo(bmwXetraOpen) < 0
                         || ts.toLocalTime().compareTo(bmwXetraClose) > 0);
@@ -157,37 +157,52 @@ public class TwitterCorpusListImpl implements TwitterCorpus {
      * @param labels
      */
     public void labelCorpus(PriceLabelCorpus labels){
-        LocalTime bmwXetraOpen = LocalTime.of(8,0,0);		// London time
-        LocalTime bmwXetraClose = LocalTime.of(16,35,0);	// London time
         Iterator<Tweet> corpusIterator = corpus.iterator();
         while(corpusIterator.hasNext()){
             Tweet focus = corpusIterator.next();
             ZonedDateTime focusTS = focus.getTimeStamp();
+            ZonedDateTime lastPrintBeforeTweet = null;
+            ZonedDateTime twentyMinsAfterTweet = null;
             if(focusTS.minusMinutes(1).toLocalTime().compareTo(bmwXetraOpen) < 0){
-
-                // compare previous day's close to the next day's open price
-
-            } else if(focusTS.plusMinutes(20).toLocalTime().compareTo(bmwXetraClose) > 0){
-
-                // compare previous day's close to the next day's open price
-
-            } else {
-                ZonedDateTime lastPrintBeforeTweet = focusTS.minusMinutes(1);
-                ZonedDateTime twentyMinsAfterTweet = focusTS.plusMinutes(20);
-                PriceSnapshot openingSnap = labels.getPriceMap().get(lastPrintBeforeTweet);
-                PriceSnapshot closingSnap = labels.getPriceMap().get(twentyMinsAfterTweet);
-                focus.setInitialSnapshot(openingSnap);
-                focus.setPostTweetSnapshot(closingSnap);
-                focus.setIsLabelled(true);
-                if(focus.getInitialSnapshot().getOpeningSharePrice() > focus.getPostTweetSnapshot().getClosingSharePrice()){
-                    focus.setSentiment(Sentiment.NEGATIVE);
-                } else if(focus.getInitialSnapshot().getOpeningSharePrice() < focus.getPostTweetSnapshot().getClosingSharePrice()) {
-                    focus.setSentiment(Sentiment.POSITIVE);
+                // time stamp of the tweet occurs before or on market open
+                if(focusTS.toLocalTime().plusMinutes(20).compareTo(bmwXetraOpen) >= 0){
+                    // time stamp of the tweet occurs within the 20 minutes prior to the market open --> compare previous day's close to the price 20 mins after the tweet is published
+                    lastPrintBeforeTweet = ZonedDateTime.of(bmwXetraClose.atDate(focusTS.toLocalDate().minusDays(1)),ZoneId.of("Europe/London"));
+                    twentyMinsAfterTweet = focusTS.plusMinutes(20);
                 } else {
-                    focus.setSentiment(Sentiment.NEUTRAL);
+                    // time stamp of the tweet occurs more than 20 minutes prior to the market open --> compare previous day's close to the next day's open price
+                    lastPrintBeforeTweet = ZonedDateTime.of(bmwXetraClose.atDate(focusTS.toLocalDate().minusDays(1)), ZoneId.of("Europe/London"));
+                    twentyMinsAfterTweet = ZonedDateTime.of(bmwXetraOpen.atDate(focusTS.toLocalDate()), ZoneId.of("Europe/London"));
                 }
+            } else if(focusTS.plusMinutes(20).toLocalTime().compareTo(bmwXetraClose) > 0){
+                // time stamp of the tweet occurs at a time later than 20 minutes before the market close
+                if(focusTS.toLocalTime().compareTo(bmwXetraClose) > 0){
+                    // time stamp of the tweet occurs after the market close --> compare  today's close to the next day's open price
+                    lastPrintBeforeTweet = ZonedDateTime.of(bmwXetraClose.atDate(focusTS.toLocalDate()),ZoneId.of("Europe/London"));
+                    twentyMinsAfterTweet = ZonedDateTime.of(bmwXetraOpen.atDate(focusTS.toLocalDate().plusDays(1)),ZoneId.of("Europe/London"));
+                } else {
+                    // time stamp of the tweet occurs within 20 minutes of the market close --> compare the price at time of the tweets release to the next day's open price
+                    lastPrintBeforeTweet = focusTS.minusMinutes(1);
+                    twentyMinsAfterTweet = ZonedDateTime.of(bmwXetraOpen.atDate(focusTS.toLocalDate().plusDays(1)),ZoneId.of("Europe/London"));
+                }
+            } else {
+                // timestamp of the tweet occurs during normal market hours
+                lastPrintBeforeTweet = focusTS.minusMinutes(1);
+                twentyMinsAfterTweet = focusTS.plusMinutes(20);
             }
-
+            PriceSnapshot openingSnap = labels.getPriceMap().get(lastPrintBeforeTweet);
+            PriceSnapshot closingSnap = labels.getPriceMap().get(twentyMinsAfterTweet);
+            focus.setInitialSnapshot(openingSnap);
+            focus.setPostTweetSnapshot(closingSnap);
+            focus.setIsLabelled(true);
+            // compare the two market snapshots to discern the implied market sentiment of the tweet
+            if(focus.getInitialSnapshot().getOpeningSharePrice() > focus.getPostTweetSnapshot().getClosingSharePrice()){
+                focus.setSentiment(Sentiment.NEGATIVE);
+            } else if(focus.getInitialSnapshot().getOpeningSharePrice() < focus.getPostTweetSnapshot().getClosingSharePrice()) {
+                focus.setSentiment(Sentiment.POSITIVE);
+            } else {
+                focus.setSentiment(Sentiment.NEUTRAL);
+            }
         }
     }
 
