779e682b7a96c5b23fd0d2eecf22fbd73e11c380
Andrew Ho <andrewyc.ho@gmail.com>
2015-06-21 15:44:24 +0100

    changed the code in testExtractPriceDataFromFile() method to guard against null pointers

--- TFS/src/twittercorpus/PriceLabelCorpusImplTest.java
+++ TFS/src/twittercorpus/PriceLabelCorpusImplTest.java
@@ -21,7 +21,7 @@ public class PriceLabelCorpusImplTest {
 
     @Before
     public void setUp() throws Exception {
-        plFilename = "/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/BMW Price Data - April 2015.txt";
+        plFilename = "/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/Test Price Data Sample.txt";
         plCorpus = new PriceLabelCorpusImpl(plFilename);
         plCorpus.extractPriceDataFromFile(plFilename);
     }
@@ -39,7 +39,7 @@ public class PriceLabelCorpusImplTest {
 
     @Test
     public void testGetFileName() throws Exception {
-        assertEquals("/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/BMW Price Data - April 2015.txt",plCorpus.getFileName());
+        assertEquals("/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/Test Price Data Sample.txt",plCorpus.getFileName());
     }
 
     @Test
@@ -54,12 +54,16 @@ public class PriceLabelCorpusImplTest {
         // search on data entry: 30/04/15 16:35	106.1	106.1	106.1	106.1	0.0403	0.0584	-0.0181
         LocalDateTime localPlTS = LocalDateTime.of(2015,4,30,16,35,0);
         ZonedDateTime plTS = ZonedDateTime.of(localPlTS, ZoneId.of("Europe/London"));
-        assertTrue(Math.abs(plCorpus.getPriceMap().get(plTS).getClosingSharePrice() - 106.1) < 0.0000001);
+        assertNotNull(plCorpus.getPriceMap().get(plTS));
+        if(plCorpus.getPriceMap().get(plTS) != null)
+            assertTrue(Math.abs(plCorpus.getPriceMap().get(plTS).getClosingSharePrice() - 106.1) < 0.0000001);
 
         // search on data entry:  07/04/15 08:32	115.65	115.7	115.5	115.6	0.0443	0.0049	0.0394
         localPlTS = LocalDateTime.of(2015,4,7,8,32,0);
         plTS = ZonedDateTime.of(localPlTS, ZoneId.of("Europe/London"));
-        assertTrue(Math.abs(plCorpus.getPriceMap().get(plTS).getOpeningMACDDirectionSignal() - 0.0394) < 0.0000001);
+        assertNotNull(plCorpus.getPriceMap().get(plTS));
+        if(plCorpus.getPriceMap().get(plTS) != null)
+            assertTrue(Math.abs(plCorpus.getPriceMap().get(plTS).getOpeningMACDDirectionSignal() - 0.0394) < 0.0000001);
     }
 
 }
