1bdee5c55e2d6eed02bff125d192956d39500915
Andrew Ho <andrewyc.ho@gmail.com>
2015-06-21 12:36:30 +0100

    wrote code for testExtractPriceDataFromFile() test class
    tested code and it passed

--- TFS/src/twittercorpus/PriceLabelCorpusImplTest.java
+++ TFS/src/twittercorpus/PriceLabelCorpusImplTest.java
@@ -51,24 +51,28 @@ public class PriceLabelCorpusImplTest {
         // search on data entry: 30/04/15 16:35	106.1	106.1	106.1	106.1	0.0403	0.0584	-0.0181
         LocalDateTime localPlTS = LocalDateTime.of(2015,4,30,16,35,0);
         ZonedDateTime plTS = ZonedDateTime.of(localPlTS, ZoneId.of("Europe/London"));
-        System.out.println(plCorpus.getPriceMap().containsKey(plTS));
-        Iterator<ZonedDateTime> keyIt = plCorpus.getPriceMap().keySet().iterator();
-        if(keyIt.hasNext()){
-            ZonedDateTime aKeyZDT = keyIt.next();
-            System.out.println(aKeyZDT);
-            int d = aKeyZDT.getDayOfMonth();
-            System.out.println(d);
-            int m = aKeyZDT.getMonthValue();
-            System.out.println(m);
-            int y = aKeyZDT.getYear();
-            System.out.println(y);
-            int h = aKeyZDT.getHour();
-            System.out.println(h);
-            int min = aKeyZDT.getMinute();
-            System.out.println(min);
-            ZoneId z = aKeyZDT.getZone();
-            System.out.println(z);
-        }
-        //assertTrue(plCorpus.getPriceMap().get(plTS).getClosingSharePrice() - 106.1 < 0.0000001);
+//        Iterator<ZonedDateTime> keyIt = plCorpus.getPriceMap().keySet().iterator();
+//        if(keyIt.hasNext()){
+//            ZonedDateTime aKeyZDT = keyIt.next();
+//            System.out.println(aKeyZDT);
+//            int d = aKeyZDT.getDayOfMonth();
+//            System.out.println(d);
+//            int m = aKeyZDT.getMonthValue();
+//            System.out.println(m);
+//            int y = aKeyZDT.getYear();
+//            System.out.println(y);
+//            int h = aKeyZDT.getHour();
+//            System.out.println(h);
+//            int min = aKeyZDT.getMinute();
+//            System.out.println(min);
+//            ZoneId z = aKeyZDT.getZone();
+//            System.out.println(z);
+//        }
+        assertTrue(Math.abs(plCorpus.getPriceMap().get(plTS).getClosingSharePrice() - 106.1) < 0.0000001);
+
+        // search on data entry:  07/04/15 08:32	115.65	115.7	115.5	115.6	0.0443	0.0049	0.0394
+        localPlTS = LocalDateTime.of(2015,4,7,8,32,0);
+        plTS = ZonedDateTime.of(localPlTS, ZoneId.of("Europe/London"));
+        assertTrue(Math.abs(plCorpus.getPriceMap().get(plTS).getOpeningMACDDirectionSignal() - 0.0394) < 0.0000001);
     }
 }
