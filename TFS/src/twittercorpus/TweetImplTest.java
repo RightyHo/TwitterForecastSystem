a4dc50d72bd0cbade77cd24aa778e319dcdf57a8
Andrew Ho <andrewyc.ho@gmail.com>
2015-06-30 22:27:41 +0100

    started writing TweetImplTest class

--- TFS/src/twittercorpus/TweetImplTest.java
+++ TFS/src/twittercorpus/TweetImplTest.java
@@ -3,6 +3,10 @@ package twittercorpus;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+
 import static org.junit.Assert.*;
 
 /**
@@ -10,13 +14,25 @@ import static org.junit.Assert.*;
  */
 public class TweetImplTest {
 
+    private Tweet testTw;
+
     @Before
     public void setUp() throws Exception {
-
+        // Tweet example Fri Jan 16 07:26:00 2015	my family doesn't own a car that's not a beemer.
+        LocalDateTime lDateT = LocalDateTime.of(2015,1,16,7,26,0);
+        ZonedDateTime tTime = ZonedDateTime.of(lDateT, ZoneId.of("Europe/London"));
+        testTw = new TweetImpl(tTime,"my family doesn't own a car that's not a beemer.");
     }
 
     @Test
     public void testExtractNGramFeatures() throws Exception {
+
+        // check whether unigram features are extracted as expected
+        testTw.extractNGramFeatures(1,testTw.getTweetText());
+
+
+        // check whether bigram features are extracted as expected
+        testTw.extractNGramFeatures(1,testTw.getTweetText());
 
     }
 }
