794d06d7585f5ccfd8dc54f8640e4388d804635f
Andrew Ho <andrewyc.ho@gmail.com>
2015-07-05 19:49:11 +0300

    wrote code for ClassificationImpl class
    need to add some tests

--- TFS/src/twittercorpus/ClassificationImpl.java
+++ TFS/src/twittercorpus/ClassificationImpl.java
@@ -7,28 +7,53 @@ import java.util.List;
  */
 public class ClassificationImpl implements Classification {
 
+    private List<String> listOfFeatures;
+    private Sentiment sentimentCategory;
+    double classificationCertainty;
+
+    /**
+     * Constructor with all parameters passed to it.  Might be worthwhile adding a constructor
+     * with a classificationCertainty default set to 1.0?
+     * @param listOfFeatures
+     * @param sentimentCategory
+     * @param classificationCertainty
+     */
+    public ClassificationImpl(List<String> listOfFeatures, Sentiment sentimentCategory, double classificationCertainty) {
+        this.listOfFeatures = listOfFeatures;
+        this.sentimentCategory = sentimentCategory;
+        this.classificationCertainty = classificationCertainty;
+    }
+
     /**
      * getter for listOfFeatures field
      * @return the list of features held in the classification object
      */
-    List<String> getListOfFeatures();
+    public List<String> getListOfFeatures(){
+        return null;
+    }
 
     /**
      * getter for sentimentCategory field
      * @return the sentiment category this list of features was classified under
      *
      */
-    Sentiment getSentimentCategory();
+    public Sentiment getSentimentCategory(){
+        return null;
+    }
 
     /**
      * getter for the classificationCertainty field
      * @return the probability that the list of features has been classified in the correct category
      */
-    double getClassificationCertainty();
+    public double getClassificationCertainty(){
+        return 0.0;
+    }
 
     /**
      * returns a nicely formatted string
      * @return a formatted string depicting the object
      */
-    String toString();
+    public String toString(){
+        return null;
+    }
 }
