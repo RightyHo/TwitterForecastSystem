5b3f2ad33ea9c839b1682499f446b230dd65587e
Andrew Ho <andrewyc.ho@gmail.com>
2015-07-27 08:17:33 +0100

    wrote new PredictionStatistics Interface

---
+++ TFS/src/twittercorpus/PredictionStatistics.java
@@ -0,0 +1,15 @@
+package twittercorpus;
+
+import java.util.List;
+
+/**
+ * Created by Andrew on 27/07/15.
+ */
+public interface PredictionStatistics {
+
+    void calculateTFSAccuracy(List<Tweet> testData);
+
+    void calculateMACDAccuracy(List<Tweet> testData);
+
+    void calculateSentiWordNetAccuracy(List<Tweet> testData);
+}
