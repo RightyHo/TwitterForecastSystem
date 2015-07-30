c6d0eed6a3bf37754d3c75ba4c102790f952c798
Andrew Ho <andrewyc.ho@gmail.com>
2015-07-30 21:42:27 +0100

    added code to set all the calculations flags to true once they were completed and the results printed out nicely for the TFS and MACD

--- TFS/src/twittercorpus/PredictionStatisticsImpl.java
+++ TFS/src/twittercorpus/PredictionStatisticsImpl.java
@@ -63,6 +63,7 @@ public class PredictionStatisticsImpl implements PredictionStatistics {
                 throw new IllegalArgumentException("The test data has not been correctly classified");
             }
         }
+        madeTFSCalcs = true;
     }
 
     public void calculateMACDAccuracy(List<Tweet> testData){
@@ -97,6 +98,7 @@ public class PredictionStatisticsImpl implements PredictionStatistics {
                 }
             }
         }
+        madeMACDCalcs = true;
     }
 
     public void calculateSentiWordNetAccuracy(List<Tweet> testData){
@@ -106,23 +108,24 @@ public class PredictionStatisticsImpl implements PredictionStatistics {
 
     public void printResults(){
         System.out.println("Size of Test Data Set:  " + sizeOfTestDataSet);
-        if(madeTFSCalcs){
+        if(madeTFSCalcs) {
             System.out.println("Number of Correct TFS Predictions: " + correctTFSCount);
             System.out.println("Number of Incorrect TFS Predictions: " + incorrectTFSCount);
             System.out.println("Overall Accuracy of TFS Predictions: " + String.valueOf((double) correctTFSCount / (double) sizeOfTestDataSet));
-            if(correctTFSCount + incorrectTFSCount != sizeOfTestDataSet) throw new IllegalArgumentException("TFS calculations are inconsistent");
-        } else if(madeMACDCalcs) {
+            if (correctTFSCount + incorrectTFSCount != sizeOfTestDataSet)
+                throw new IllegalArgumentException("TFS calculations are inconsistent");
+        }
+        if(madeMACDCalcs) {
             System.out.println("Number of Correct MACD Predictions: " + correctMACDCount);
             System.out.println("Number of Incorrect MACD Predictions: " + incorrectMACDCount);
             System.out.println("Overall Accuracy of MACD Predictions: " + String.valueOf((double) correctMACDCount / (double) sizeOfTestDataSet));
             if (correctMACDCount + incorrectMACDCount != sizeOfTestDataSet) throw new IllegalArgumentException("MACD calculations are inconsistent");
-        } else if(madeSentiWordNetCalcs) {
+        }
+        if(madeSentiWordNetCalcs) {
             System.out.println("Number of Correct SentiWordNet Predictions: " + correctSentiWordNetCount);
             System.out.println("Number of Incorrect SentiWordNet Predictions: " + incorrectSentiWordNetCount);
             System.out.println("Overall Accuracy of SentiWordNet Predictions: " + String.valueOf((double) correctSentiWordNetCount / (double) sizeOfTestDataSet));
             if (correctSentiWordNetCount + incorrectSentiWordNetCount != sizeOfTestDataSet) throw new IllegalArgumentException("SentiWordNet calculations are inconsistent");
         }
-
-
     }
 }
