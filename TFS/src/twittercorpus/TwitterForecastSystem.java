f4a04766e8b110cb330f87312c1b3a6fba0aaeff
Andrew Ho <andrewyc.ho@gmail.com>
2015-08-02 17:21:49 +0100

    changed input files to test the system on more data

--- TFS/src/twittercorpus/TwitterForecastSystem.java
+++ TFS/src/twittercorpus/TwitterForecastSystem.java
@@ -18,8 +18,8 @@ public class TwitterForecastSystem {
     private List<Tweet> testData = null;
     private Classifier classifier = null;
 
-    public static final String TWITTER_CORPUS_FILENAME = "/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/Test Twitter Corpus Sample - New Date Format.txt";
-    public static final String PRICE_DATA_FILENAME = "/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/Test Price Data Sample.txt";
+    public static final String TWITTER_CORPUS_FILENAME = "/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/FirstBusinessWeekOfFeb2015BmwTwitterCorpus.txt";
+    public static final String PRICE_DATA_FILENAME = "/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/BMW GY and MACD Minute Price Bars in GMT February 2015.txt";
     public static final String SENTIWORDNET_FILENAME = "/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/SentiWordNet_3.0.txt";
     public static final String ABBREVIATION_DICTIONARY_FILENAME = "/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/Twerminology - 100 Twitter Slang Words & Abbreviations.txt";
     public static final String SPELLING_DICTIONARY_FILENAME = "/Users/Andrew/Documents/Programming/MSc Project/Natural Language Processing/Project Data Sets/dictionary.txt";
